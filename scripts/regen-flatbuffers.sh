126255185ab918684f05a6840acdd4667258319f
root <joseprupi@gmail.com>
2026-01-31 02:11:18 +0000

    Trying flatc serializer, not working properly

--- scripts/regen-flatbuffers.sh
+++ scripts/regen-flatbuffers.sh
@@ -10,9 +10,9 @@ for fbs in flatbuffers/fbs/*.fbs; do
     flatc --cpp -o flatbuffers/cpp/ "$fbs"
 done
 
-# Python generation with --gen-all
-echo "Generating Python code..."
-flatc --python --gen-all -o flatbuffers/python \
+# Python generation with --gen-all and --gen-object-api
+echo "Generating Python code with Object API..."
+flatc --python --gen-all --gen-object-api -o flatbuffers/python \
     flatbuffers/fbs/price_fixed_rate_bond_request.fbs \
     flatbuffers/fbs/price_floating_rate_bond_request.fbs \
     flatbuffers/fbs/price_vanilla_swap_request.fbs \
